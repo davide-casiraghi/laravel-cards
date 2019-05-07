dea3e20178c1c5b8e013924418638070ec43e99c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-07 19:37:54 +0200

    start testing it_gets_the_parameter_array

--- tests/LaravelCardsTest.php
+++ tests/LaravelCardsTest.php
@@ -28,23 +28,39 @@ class LaravelCardsTest extends TestCase
         $text = "Lorem ipsum {# card post_id=[6] img_alignment=[right] img_col_size=[3] bkg_color=[#345642] text_color=[#212529] container_wrap=[false] #} sid amet.
                  Lorem ipsum {# card post_id=[8] img_alignment=[left] img_col_size=[2] bkg_color=[#FF0044] text_color=[#f34532] container_wrap=[true] #}.
         ";
-        $snippet_occurances = LaravelCards::getCardSnippetOccurrences($text);
-        //dd($snippet_occurances);
+        $matches = LaravelCards::getCardSnippetOccurrences($text);
+        //dd($matches);
         
-        $this->assertEquals(6, $snippet_occurances[0][2]);
-        $this->assertEquals('right', $snippet_occurances[0][4]);
-        $this->assertEquals(3, $snippet_occurances[0][6]);
-        $this->assertEquals('#345642', $snippet_occurances[0][8]);
-        $this->assertEquals('#212529', $snippet_occurances[0][10]);
-        $this->assertEquals('false', $snippet_occurances[0][12]);  
+        $this->assertEquals(6, $matches[0][2]);
+        $this->assertEquals('right', $matches[0][4]);
+        $this->assertEquals(3, $matches[0][6]);
+        $this->assertEquals('#345642', $matches[0][8]);
+        $this->assertEquals('#212529', $matches[0][10]);
+        $this->assertEquals('false', $matches[0][12]);  
         
-        $this->assertEquals(8, $snippet_occurances[1][2]);
-        $this->assertEquals('left', $snippet_occurances[1][4]);
-        $this->assertEquals(2, $snippet_occurances[1][6]);
-        $this->assertEquals('#FF0044', $snippet_occurances[1][8]);
-        $this->assertEquals('#f34532', $snippet_occurances[1][10]);
-        $this->assertEquals('true', $snippet_occurances[1][12]); 
-          
+        $this->assertEquals(8, $matches[1][2]);
+        $this->assertEquals('left', $matches[1][4]);
+        $this->assertEquals(2, $matches[1][6]);
+        $this->assertEquals('#FF0044', $matches[1][8]);
+        $this->assertEquals('#f34532', $matches[1][10]);
+        $this->assertEquals('true', $matches[1][12]); 
     }
+    
+    /** @test */
+    public function it_gets_the_parameter_array()
+    {
+        $text = "Lorem ipsum {# card post_id=[6] img_alignment=[right] img_col_size=[3] bkg_color=[#345642] text_color=[#212529] container_wrap=[false] #} sid amet.
+                 Lorem ipsum {# card post_id=[8] img_alignment=[left] img_col_size=[2] bkg_color=[#FF0044] text_color=[#f34532] container_wrap=[true] #}.
+        ";
+        $matches = LaravelCards::getCardSnippetOccurrences($text);
+        $parameters = LaravelCards::getParameters($matches);
+        
+        dd($parameters);
+        
+        //$this->assertEquals(6, $matches[0][2]);
+        
+    }
+    
+    
     
 }
