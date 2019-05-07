3348f4ae61b5425bdc8b487621442a8a5542f807
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-07 18:06:01 +0200

    added travis and scrutinizer config and snippet function

--- src/LaravelCards.php
+++ src/LaravelCards.php
@@ -4,5 +4,28 @@ namespace DavideCasiraghi\LaravelCards;
 
 class LaravelCards
 {
-    // Build your next great package.
+    /************************************************************************/
+
+    /**
+     *  Find the card snippet occurances in the text.
+     *  @param array $text     The text where to find the card snippets
+     *  @return array $ret     The matches
+     **/
+    public function getCardSnippetOccurrences($text)
+    {
+        $re = '/{\#
+                \h+card
+                \h+(src|column_width|gutter)=\[([^]]*)]
+                \h+((?1))=\[([^]]*)]
+                \h+((?1))=\[([^]]*)]
+                \h*\#}/x';
+
+        if (preg_match_all($re, $text, $matches, PREG_SET_ORDER, 0)) {
+            return $matches;
+        } else {
+            return;
+        }
+    }
+    
+    
 }
