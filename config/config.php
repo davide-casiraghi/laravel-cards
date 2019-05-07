29b83b4220cffd3c45034fc4426f4fa9c0770598
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-07 19:52:27 +0200

    start adding test model

--- config/config.php
+++ config/config.php
@@ -4,5 +4,7 @@
  * You can place your custom package configuration in here.
  */
 return [
-    'model_name' => 'App\Post'
+    'model' => 'App\Post',
+    'title_field' => 'title',
+    'body_field' => 'text',
 ];
