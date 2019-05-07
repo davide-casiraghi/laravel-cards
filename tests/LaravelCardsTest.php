69b05ff85130b172bc8acb1116b32988b0cd1934
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-07 18:23:22 +0200

    created test file

---
+++ tests/LaravelCardsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DavideCasiraghi\LaravelCards\Tests;
+
+use Orchestra\Testbench\TestCase;
+use DavideCasiraghi\LaravelCards\Facades\LaravelCards;
+use DavideCasiraghi\LaravelCards\LaravelCardsServiceProvider;
+
+class LaravelCardsTest extends TestCase
+{
+    
+}
