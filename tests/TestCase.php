137928d9eed918e1701bdd2898bb43abc85e0bd5
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-08 11:25:56 +0200

    added test case

---
+++ tests/TestCase.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace DavideCasiraghi\LaravelCards\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use DavideCasiraghi\LaravelCards\Facades\LaravelCards;
+use DavideCasiraghi\LaravelCards\LaravelCardsServiceProvider;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadLaravelMigrations(['--database' => 'testbench']);
+        $this->withFactories(__DIR__.'/database/factories');
+        
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelCardsServiceProvider::class,
+            //\Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
+            //\Dimsav\Translatable\TranslatableServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'LaravelCards' => LaravelCards::class, // facade called PhpResponsiveQuote and the name of the facade class
+            //'LaravelLocalization' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::class,
+        ];
+    }
+
+}
