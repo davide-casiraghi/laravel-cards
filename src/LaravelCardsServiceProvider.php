235c4e1af2c49ff88152d9f2bc0adbadaa26be63
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-03 13:07:19 +0200

    fixed card tables creation

--- src/LaravelCardsServiceProvider.php
+++ src/LaravelCardsServiceProvider.php
@@ -23,7 +23,7 @@ class LaravelCardsServiceProvider extends ServiceProvider
 
         if (! class_exists('CreateCardsTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_cards_table_table.php.stub' => database_path('migrations/'.Carbon::now()->format('Y_m_d_Hmsu').'_create_cards_table_table.php'),
+                __DIR__.'/../database/migrations/create_cards_table.php.stub' => database_path('migrations/'.Carbon::now()->format('Y_m_d_Hmsu').'_create_cards_table.php'),
             ], 'migrations');
         }
         if (! class_exists('CreateCardTranslationsTable')) {
