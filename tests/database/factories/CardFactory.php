7d7b66ea70ee9bf2091fed26d9f51eb55f494613
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-04 17:59:56 +0200

    added db migrations for tests

--- tests/database/factories/CardFactory.php
+++ tests/database/factories/CardFactory.php
@@ -14,10 +14,20 @@ use Faker\Generator as Faker;
 
 $factory->define(DavideCasiraghi\LaravelCards\Models\Card::class, function (Faker $faker) {
     return [
-        'heading' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-        'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-        'body' => $faker->paragraph,
-        'image' => 'testImage.jpg',
-        'image_alt' => $faker->sentence($nbWords = 5, $variableNbWords = true),
+        'heading:en' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'title:en' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'body:en' => $faker->paragraph,
+        'title:en' => $faker->sentence($nbWords = 5, $variableNbWords = true),
+        'button_text:en' => $faker->sentence($nbWords = 1, $variableNbWords = true),
+        'image_file_name' => 'testImage.jpg',
+        'img_alignment' => '1',
+        'img_col_size' => '200px',
+        'bkg_color' => '#FF00FF',
+        'text_color' => '#AA55AA',
+        'button_url' => 'https://www.google.it',
+        'button_color' => '#FF00BB',
+        'button_corners' => '2',
+        'button_icon' => '2',
+        'container_wrap' => false,
     ];
 });
