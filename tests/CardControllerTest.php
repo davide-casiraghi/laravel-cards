1845cb9f19550439c6bdc6761d60869cfeab03af
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-07 08:00:49 +0200

    the_route_update_can_be_accessed

--- tests/CardControllerTest.php
+++ tests/CardControllerTest.php
@@ -15,7 +15,7 @@ class CardControllerTest extends TestCase
     /***************************************************************/
 
     /** @test */
-    public function it_displays_the_cards_index_page()
+    public function the_route_index_can_be_accessed()
     {
         $this->authenticateAsAdmin();
         $this->get('laravel-cards')
@@ -24,12 +24,68 @@ class CardControllerTest extends TestCase
     }
     
     /** @test */
-    public function it_displays_the_card_create_page()
+    public function the_route_create_can_be_accessed()
     {
         $this->authenticateAsAdmin();
         $this->get('laravel-cards/create')
             ->assertViewIs('laravel-cards::cards.create')
             ->assertStatus(200);
+    }
+    
+    /** @test */
+    public function the_route_destroy_can_be_accessed()
+    {
+        $id = Card::insertGetId([
+            'image_file_name' => 'test image name',
+            'button_url' => 'test button url',
+            'img_col_size'  => '3',
+            'bkg_color'  => '#FF00FF',
+            'text_color'  => '#2365AA',
+            'container_wrap'  => '1',
+        ]);
+
+        CardTranslation::insert([
+            'card_id' => $id,
+            'heading' => 'test heading',
+            'title' => 'test title',
+            'body' => 'test body',
+            'button_text' => 'test button text',
+            'locale' => 'en',
+        ]);
+
+        $this->delete('laravel-cards/1')
+            ->assertStatus(302);
+    }
+    
+    /** @test */
+    public function the_route_update_can_be_accessed()
+    {
+        $id = Card::insertGetId([
+            'image_file_name' => 'test image name',
+            'button_url' => 'test button url',
+            'img_col_size'  => '3',
+            'bkg_color'  => '#FF00FF',
+            'text_color'  => '#2365AA',
+            'container_wrap'  => '1',
+        ]);
+
+        CardTranslation::insert([
+            'card_id' => $id,
+            'heading' => 'test heading',
+            'title' => 'test title',
+            'body' => 'test body',
+            'button_text' => 'test button text',
+            'locale' => 'en',
+        ]);
+
+        $request = new \Illuminate\Http\Request();
+        $request->replace([
+              'title' => 'test title updated',
+              'body' => 'test body updated',
+          ]);
+
+        $this->put('laravel-cards/1', [$request, 1])
+             ->assertStatus(302);
     }
     
 }
